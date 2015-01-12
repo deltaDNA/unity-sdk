a1af51cdb0caf4b63178d18ea921b207265c123e
David White <david.white@gamesanalytics.com>
2015-01-12 11:49:12 +0000

    Added TestEventUpload to test event store uploaded events correctly.

---
+++ Assets/Scripts/IntegrationTests/TestEventUpload.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using UnityEngine;
+using UnityTest;
+
+namespace DeltaDNA {
+
+class TestEventUpload : MonoBehaviour {
+
+	public void Start() {
+	
+		SDK.Instance.Settings.DebugMode = true;
+		SDK.Instance.Settings.BackgroundEventUpload = false;
+		SDK.Instance.Settings.OnFirstRunSendNewPlayerEvent = false;
+		SDK.Instance.Settings.OnInitSendClientDeviceEvent = false;
+		SDK.Instance.Settings.OnInitSendGameStartedEvent = false;
+		
+		SDK.Instance.ClearPersistentData();
+		
+		SDK.Instance.ClientVersion = "1.0";
+		SDK.Instance.HashSecret = "1VLjWqChV2YC1sJ4EPKGzSF3TbhS26hq";
+		SDK.Instance.StartSDK(
+			"76410301326725846610230818914037", 					// Environment Key	(UnitySDK)		
+			"collect2470ntysd.deltadna.net/collect/api",			// Collect URI
+			"engage2470ntysd.deltadna.net",							// Engage URI
+			"TestEventUpload"
+		);
+			
+		StartCoroutine(RunTestCoroutine());
+	
+	}
+	
+	private IEnumerator RunTestCoroutine() {
+	
+		SDK.Instance.Upload();
+		SDK.Instance.RecordEvent("testUploadEvent1");
+		
+		while (SDK.Instance.IsUploading) {
+			yield return new WaitForSeconds(1);
+		}
+		
+		SDK.Instance.Upload();
+		SDK.Instance.RecordEvent("testUploadEvent2");
+		
+		while (SDK.Instance.IsUploading) {
+			yield return new WaitForSeconds(1);
+		}
+		
+		SDK.Instance.Upload();
+		SDK.Instance.RecordEvent("testUploadEvent3");
+		
+		while (SDK.Instance.IsUploading) {
+			yield return new WaitForSeconds(1);
+		}
+		
+		SDK.Instance.Upload();
+		
+		while (SDK.Instance.IsUploading) {
+			yield return new WaitForSeconds(1);
+		}
+	
+		IntegrationTest.Pass(gameObject);
+	}
+};
+
+}
