0d81fab46724fd5dfaafad4cadc883c3c4501a6d
David White <david.white@gamesanalytics.com>
2015-05-22 13:36:07 +0100

    Added function to build android and export the unity project.

---
+++ Assets/DeltaDNA/Editor/BuildTools/BuildScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEditor;
+
+public class BuildScript {
+
+	static void BuildAndroidEclipseProject()
+	{
+		string[] scenes = { "Assets/DeltaDNA/Example/deltaDNA.unity" };
+		string outputPath = "Build/Android";
+		BuildPipeline.BuildPlayer(scenes, outputPath, BuildTarget.Android, BuildOptions.AcceptExternalModificationsToPlayer | BuildOptions.Development);
+	}
+}
