2283cb4f6767387ff95efbb17dbb510b9ca3c1b5
David White <david.white@deltadna.com>
2016-03-24 14:51:11 +0000

    Updated version to 4.1.0-beta.1

--- Assets/DeltaDNA/Helpers/Settings.cs
+++ Assets/DeltaDNA/Helpers/Settings.cs
@@ -18,7 +18,7 @@ namespace DeltaDNA
 {
     public class Settings
     {
-        internal static readonly string SDK_VERSION = "Unity SDK v4.0.0";
+        internal static readonly string SDK_VERSION = "Unity SDK v4.1.0-beta.1";
 
         internal static readonly string ENGAGE_API_VERSION = "4";
 
