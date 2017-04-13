6dc9bc4410826fe0a77e8f36c0657998409906cc
David White <david.white@deltadna.com>
2017-04-13 14:38:19 +0100

    Redirect cocoapods output to Unity console.

--- Assets/DeltaDNAAds/Editor/iOS/PostProcessBuild.cs
+++ Assets/DeltaDNAAds/Editor/iOS/PostProcessBuild.cs
@@ -32,7 +32,6 @@ public class PostProcessBuild {
     // Big number to avoid conflicting with Unity's internal ads PostProcess
     [PostProcessBuild(100000)]
     public static void OnPostprocessBuild(BuildTarget buildTarget, string path) {
-        UnityEngine.Debug.Log("OnPostprocessBuild "+buildTarget+" "+path);
 
         if (buildTarget == BuildTarget.iOS) {
 
@@ -41,10 +40,7 @@ public class PostProcessBuild {
             PBXProject project = new PBXProject();
             project.ReadFromFile(projectPath);
 
-            UnityEngine.Debug.Log(PBXProject.GetUnityTargetName());
             string target = project.TargetGuidByName(PBXProject.GetUnityTargetName());
-
-            UnityEngine.Debug.Log(target);
 
             // Add build settings for CocoaPods
             project.AddBuildProperty(target, "HEADER_SEARCH_PATHS", "$(inherited)");
@@ -56,8 +52,6 @@ public class PostProcessBuild {
             project.SetBuildProperty(target, "ENABLE_BITCODE", "NO");
 
             // Copy Podfile into project
-            string unityProjectRootPath = Path.GetFullPath("./").Normalize();
-            UnityEngine.Debug.Log(unityProjectRootPath);
 
             // Default src location is the project root
             FileUtil.ReplaceFile("Assets/DeltaDNAAds/Editor/iOS/Podfile", path + "/Podfile");
@@ -66,7 +60,6 @@ public class PostProcessBuild {
             #if UNITY_5_5_OR_NEWER
             var targetOSVersion = PlayerSettings.iOS.targetOSVersionString;
             string iosPlatform = targetOSVersion.ToString();
-            UnityEngine.Debug.Log(iosPlatform);
             #else
             var targetOSVersion = PlayerSettings.iOS.targetOSVersion;
             string iosPlatform = targetOSVersion.ToString().Substring(4).Replace('_', '.');
@@ -80,12 +73,37 @@ public class PostProcessBuild {
             project.WriteToFile(projectPath);
 
             // Run pod update
-            Process proc = new Process();
-            proc.StartInfo.FileName = "/usr/local/bin/pod";
-            proc.StartInfo.Arguments = "update --project-directory=\""+path+"\"";
-            proc.StartInfo.UseShellExecute = false;
-            proc.Start();
-            proc.WaitForExit();
+            Process process = new Process();
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.StandardErrorEncoding = System.Text.Encoding.UTF8;
+            process.StartInfo.StandardOutputEncoding = System.Text.Encoding.UTF8;
+            process.StartInfo.EnvironmentVariables["LANG"] = "en_US.UTF-8";
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            process.StartInfo.CreateNoWindow = true;
+
+            EditorUtility.DisplayProgressBar(
+                    "DeltaDNA",
+                    "Please wait, downloading ad network libraries...",
+                    (float) 0.5);
+
+            process.StartInfo.FileName = "/usr/local/bin/pod";
+            process.StartInfo.Arguments = "update --project-directory=\""+path+"\"";
+            process.Start();
+            process.WaitForExit();
+
+            EditorUtility.ClearProgressBar();
+
+            // pod writes error reason to standard out
+            if (process.ExitCode != 0) {
+                UnityEngine.Debug.LogError(string.Format(
+                    "Failed to download ad network libraries due to: \n{0}\n{1}",
+                    process.StandardOutput.ReadToEnd(),
+                    process.StandardError.ReadToEnd()));
+            } else {
+                UnityEngine.Debug.Log("Successfully downloaded ad network libraries");
+            }
         }
     }
 }
