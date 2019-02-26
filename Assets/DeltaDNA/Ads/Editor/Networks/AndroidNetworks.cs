d499cbc77f36c6aba1825d76ae026e9e58f11c44
caithan.moore <caithan.moore@deltadna.com>
2019-02-26 09:58:51 +0000

    fixes smartads version number

--- Assets/DeltaDNA/Ads/Editor/Networks/AndroidNetworks.cs
+++ Assets/DeltaDNA/Ads/Editor/Networks/AndroidNetworks.cs
@@ -34,10 +34,9 @@ namespace DeltaDNA.Ads.Editor {
         }
         
         private static readonly string[] REPOS = {
-            "http://corp-vm-artifactory/artifactory/deltadna-repo",
             "https://deltadna.bintray.com/android"
         };
-        private const string VERSION = "1.11.0-SNAPSHOT";
+        private const string VERSION = "1.11.0";
         private const string PLUGINS_PATH = "Assets/Plugins/Android";
         
         public AndroidNetworks() : base("android", "Android") {}
