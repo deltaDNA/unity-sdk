6ceeb78a0303d95586d5056e135f8d39c89a9b4d
caithan.moore <caithan.moore@deltadna.com>
2019-06-21 15:17:17 +0100

    removed rogue imports

--- Assets/DeltaDNA/Triggers/EventTrigger.cs
+++ Assets/DeltaDNA/Triggers/EventTrigger.cs
@@ -17,9 +17,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using NUnit.Framework.Constraints;
-using UnityEngine;
 
 namespace DeltaDNA {
 
