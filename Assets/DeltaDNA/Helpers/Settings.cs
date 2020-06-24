45522277bd4af57bcb3659b73681208173edc0c2
Thanyaluk Jirapech-umpai <tj.jirapechumpai@unity3d.com>
2020-06-24 14:31:32 +0100

    update SDK version from v4.12.9 to v4.12.10 change the script update-version to update-sdk-version to solve the conflict on case sensitive folder.

--- Assets/DeltaDNA/Helpers/Settings.cs
+++ Assets/DeltaDNA/Helpers/Settings.cs
@@ -18,7 +18,7 @@ namespace DeltaDNA
 {
     public class Settings
     {
-        internal static readonly string SDK_VERSION = "Unity SDK v4.12.9";
+        internal static readonly string SDK_VERSION = "Unity SDK v4.12.10";
 
         internal static readonly string ENGAGE_API_VERSION = "4";
 
