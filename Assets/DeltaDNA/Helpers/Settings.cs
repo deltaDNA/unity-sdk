87a27f4755dd03c9a184b80a26f9c2e10efdc2b4
Tristan Heal <tristan.heal@unity3d.com>
2020-07-13 13:52:42 +0000

    Android FCM Fixes

--- Assets/DeltaDNA/Helpers/Settings.cs
+++ Assets/DeltaDNA/Helpers/Settings.cs
@@ -18,7 +18,7 @@ namespace DeltaDNA
 {
     public class Settings
     {
-        internal static readonly string SDK_VERSION = "Unity SDK v4.13.1";
+        internal static readonly string SDK_VERSION = "Unity SDK v4.13.2";
 
         internal static readonly string ENGAGE_API_VERSION = "4";
 
